67a60385137d9b7b599e6d3c2d791a01b1ec1b89
Kuldeep Singh <61520499+kuldeep-singh-dev@users.noreply.github.com>
2025-10-28 20:06:31 -0400

    Fix CORS allowed origins URL format

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "https://tyrepro.ca/",
-  "https://tyrepro.netlify.app/"
+  "https://tyrepro.netlify.app"
 ];
 
 const corsOptions = {
