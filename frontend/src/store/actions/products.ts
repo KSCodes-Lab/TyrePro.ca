cdc1ac84fbe9b4b6de403323c44cf2872cffc4e0
kuldeep-singh-dev <kuldeepinsan5894@gmail.com>
2025-10-31 20:39:34 -0400

    changing backend api address from local to render

--- frontend/src/store/actions/products.ts
+++ frontend/src/store/actions/products.ts
@@ -2,8 +2,8 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import type { Product } from "@/store/reducers/products";
 
-//const API_BASE = process.env.NEXT_PUBLIC_API_BASE ?? "https://tyrepro.onrender.com";
-const API_BASE = process.env.NEXT_PUBLIC_API_BASE ?? "http://localhost:5000";
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE ?? "https://tyrepro.onrender.com";
+//const API_BASE = process.env.NEXT_PUBLIC_API_BASE ?? "http://localhost:5000";
 
 export type FetchProductsArgs = {
   page?: number;
